4348eb10a69f30103017fb348bd8439bc459ec57
Yuriy-vasylevsky <80046118+Yuriy-vasylevsky@users.noreply.github.com>
2023-02-05 19:44:19 +0200

    fix deploy

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,11 +1,14 @@
 const menuBtnOpen = document.querySelector('.icon-menu');
 const mobileMenu = document.querySelector('.mobile');
 const menuBtnClose = document.querySelector('.mobile');
+const body = document.querySelector('.body');
 
 menuBtnOpen.addEventListener('click', () => {
   mobileMenu.classList.toggle('is-hiden');
+  body.classList.toggle('overflov');
 });
 
 menuBtnClose.addEventListener('click', () => {
   mobileMenu.classList.toggle('is-hiden');
+  body.classList.toggle('overflov');
 });
