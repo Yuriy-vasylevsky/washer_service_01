3354942e2806dde38dc584a0bf543cf3cb2ff555
Yuriy-vasylevsky <80046118+Yuriy-vasylevsky@users.noreply.github.com>
2023-02-07 15:21:07 +0200

    Зробив мобільне меню

--- src/index.js
+++ src/index.js
@@ -5,24 +5,31 @@ const faqList = document.querySelector('.faq__list');
 faqList.addEventListener('click', openAnsver);
 
 let faqAnsver = null;
-let ansverindex = null;
+let ansverIndex = null;
+let selectSvg = null;
+console.log('selectSvg', selectSvg);
 
 function openAnsver(e) {
   clickOnSvg(e);
   findElToOpen(e);
   openFindEl(e);
+  togleSvg(e);
 }
 
 function clickOnSvg(e) {
   if ('use' !== e.target.nodeName && 'svg' !== e.target.nodeName) {
     return;
   } else {
-    ansverindex = e.target.dataset.index;
+    selectSvg = e.target;
+    console.log('selectSvg', selectSvg);
+
+    ansverIndex = e.target.dataset.index;
+    return selectSvg;
   }
 }
 
 function findElToOpen(e) {
-  faqAnsver = document.querySelector(`#p-${ansverindex}`);
+  faqAnsver = document.querySelector(`#p-${ansverIndex}`);
   return faqAnsver;
 }
 
@@ -31,5 +38,13 @@ function openFindEl(e) {
     return;
   } else {
     faqAnsver.classList.toggle('is-open');
+  }
+}
+
+function togleSvg(e) {
+  if ('svg' !== e.target.nodeName) {
+    return;
+  } else {
+    selectSvg.classList.toggle('is-open');
   }
 }
--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,14 +1,15 @@
 const menuBtnOpen = document.querySelector('.icon-menu');
 const mobileMenu = document.querySelector('.mobile');
-const menuBtnClose = document.querySelector('.mobile');
 const body = document.querySelector('.body');
 
 menuBtnOpen.addEventListener('click', () => {
+  menuBtnOpen.classList.toggle('is-open');
   mobileMenu.classList.toggle('is-hiden');
   body.classList.toggle('overflov');
 });
 
-menuBtnClose.addEventListener('click', () => {
-  mobileMenu.classList.toggle('is-hiden');
-  body.classList.toggle('overflov');
-});
+// menuBtnOpen.addEventListener('click', () => {
+//   menuBtnOpen.classList.toggle('is-open');
+//   mobileMenu.classList.toggle('is-hiden');
+//   body.classList.toggle('overflov');
+// });
