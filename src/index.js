30e1f0b7c63963318b451ab758c0ec4f70f125e1
Yuriy-vasylevsky <80046118+Yuriy-vasylevsky@users.noreply.github.com>
2023-02-23 17:13:31 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -1,50 +1,103 @@
 import './sass/main.scss';
 import './js/mobile-menu';
 
+// const faqList = document.querySelector('.faq__list');
+// faqList.addEventListener('click', openAnsver);
+
+// let faqAnsver = null;
+// let ansverIndex = null;
+// let selectSvg = null;
+// console.log('selectSvg', selectSvg);
+
+// function openAnsver(e) {
+//   clickOnSvg(e);
+//   clickOnHeader(e);
+//   findElToOpen(e);
+//   openFindEl(e);
+//   togleSvg(e);
+// }
+
+// function clickOnSvg(e) {
+//   if ('use' !== e.target.nodeName && 'svg' !== e.target.nodeName) {
+//     return;
+//   } else {
+//     selectSvg = e.target;
+//     ansverIndex = e.target.dataset.index;
+//     // return selectSvg;
+//   }
+// }
+
+// function clickOnHeader(e) {
+//   if ('H3' !== e.target.nodeName) {
+//     return;
+//   } else {
+//     selectSvg = e.target;
+
+//     ansverIndex = e.target.dataset.index;
+
+//     return selectSvg;
+//   }
+// }
+
+// function findElToOpen(e) {
+//   faqAnsver = document.querySelector(`#p-${ansverIndex}`);
+
+//   console.log('faqAnsver', faqAnsver);
+//   return faqAnsver;
+// }
+
+// function openFindEl(e) {
+//   if ('H3' !== e.target.nodeName && 'svg' !== e.target.nodeName) {
+//     return;
+//   } else {
+//     faqAnsver.classList.toggle('is-open');
+//   }
+// }
+
+// function togleSvg(e) {
+//   if ('svg' !== e.target.nodeName) {
+//     return;
+//   } else {
+//     selectSvg.classList.toggle('is-open');
+//   }
+// }
+
 const faqList = document.querySelector('.faq__list');
-faqList.addEventListener('click', openAnsver);
+console.log('faqList', faqList);
+const faqAnswers = document.querySelectorAll('.faq__ansver');
+console.log('faqAnswers', faqAnswers);
 
-let faqAnsver = null;
-let ansverIndex = null;
-let selectSvg = null;
-console.log('selectSvg', selectSvg);
+faqList.addEventListener('click', handleFaqClick);
 
-function openAnsver(e) {
-  clickOnSvg(e);
-  findElToOpen(e);
-  openFindEl(e);
-  togleSvg(e);
-}
+function handleFaqClick(e) {
+  const targetSvg = e.target;
+  const currentSvg = e.target.nextElementSibling;
+  switch (targetSvg.tagName) {
+    case 'H3':
+      toggleAnswer(targetSvg.dataset.index);
+      toggleSvg(currentSvg);
+      break;
 
-function clickOnSvg(e) {
-  if ('use' !== e.target.nodeName && 'svg' !== e.target.nodeName) {
-    return;
-  } else {
-    selectSvg = e.target;
-    console.log('selectSvg', selectSvg);
-
-    ansverIndex = e.target.dataset.index;
-    return selectSvg;
+    case 'svg':
+      toggleAnswer(targetSvg.dataset.index);
+      toggleSvg(targetSvg);
+      break;
   }
 }
 
-function findElToOpen(e) {
-  faqAnsver = document.querySelector(`#p-${ansverIndex}`);
-  return faqAnsver;
+function toggleAnswer(index) {
+  const answer = faqAnswers[index];
+  answer.classList.toggle('is-open');
 }
 
-function openFindEl(e) {
-  if ('use' !== e.target.nodeName && 'svg' !== e.target.nodeName) {
-    return;
-  } else {
-    faqAnsver.classList.toggle('is-open');
-  }
+function toggleSvg(svg) {
+  svg.classList.toggle('is-open');
 }
 
-function togleSvg(e) {
-  if ('svg' !== e.target.nodeName) {
-    return;
-  } else {
-    selectSvg.classList.toggle('is-open');
-  }
-}
+// $('.slider').slick({
+//   dots: true,
+//   infinite: true,
+//   speed: 500,
+//   slidesToShow: 1,
+//   adaptiveHeight: true,
+// });
--- src/js/open-map.js
+++ src/js/open-map.js
@@ -1,6 +1,23 @@
 const btnOpenMap = document.querySelector('.image-button');
 const map = document.querySelector('.map-box');
 
-btnOpenMap.addEventListener('click', () => {
-  map.classList.toggle('is-hiden-non');
-});
+btnOpenMap.addEventListener('click', handelClickBtn);
+
+function handelClickBtn(e) {
+  const isOpen = btnOpenMap.dataset.open === 'true';
+  isOpen ? closeMap(e) : openMap(e);
+}
+
+function openMap(e) {
+  console.log('отрабртал опен');
+  btnOpenMap.textContent = 'Приховати карту';
+  map.classList.remove('is-hiden-non');
+  btnOpenMap.dataset.open = 'true';
+}
+
+function closeMap(e) {
+  console.log('отрабртал клоуз');
+  btnOpenMap.textContent = 'Показати карту';
+  map.classList.add('is-hiden-non');
+  btnOpenMap.dataset.open = 'false';
+}
