3d16c22340bc48aeead4cddf75033544c688697d
Yuriy-vasylevsky <80046118+Yuriy-vasylevsky@users.noreply.github.com>
2023-03-03 23:11:35 +0200

    збільшив швидкість загрузки сайту

--- src/index.js
+++ src/index.js
@@ -11,9 +11,10 @@ faqList.addEventListener('click', handleFaqClick);
 
 function handleFaqClick(e) {
   const targetSvg = e.target;
-  const currentSvg = e.target.nextElementSibling;
+
   switch (targetSvg.tagName) {
     case 'H3':
+      let currentSvg = targetSvg.nextElementSibling.querySelector('svg');
       toggleAnswer(targetSvg.dataset.index);
       toggleSvg(currentSvg);
       break;
@@ -33,42 +34,3 @@ function toggleAnswer(index) {
 function toggleSvg(svg) {
   svg.classList.toggle('is-open');
 }
-
-// $(document).ready(function () {
-//   $('.advances__list').slick({
-//     dots: true,
-//     infinite: true,
-//     speed: 500,
-//     slidesToShow: 3,
-
-//     prevArrow:
-//       '<button type="button" class="slick-prev"><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M15 18L9 12L15 6" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg></button>',
-//     nextArrow:
-//       '<button type="button" class="slick-next"><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M9 6L15 12L9 18" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg></button>',
-
-//     responsive: [
-//       {
-//         breakpoint: 1200,
-//         settings: {
-//           slidesToShow: 2,
-//           infinite: true,
-//         },
-//       },
-//       {
-//         breakpoint: 768,
-//         settings: {
-//           slidesToShow: 1,
-//           dots: true,
-//         },
-//       },
-//       {
-//         breakpoint: 480,
-//         settings: {
-//           slidesToShow: 1,
-//           // dots: true,
-//           adaptiveHeight: true,
-//         },
-//       },
-//     ],
-//   });
-// });
--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,15 +1,12 @@
 const menuBtnOpen = document.querySelector('.icon-menu');
 const mobileMenu = document.querySelector('.mobile');
 const body = document.querySelector('.body');
+const btn = document.querySelector('.header__button');
 
 menuBtnOpen.addEventListener('click', () => {
+  const expanded = btn.getAttribute('aria-expanded') === 'true' || false;
+  btn.setAttribute('aria-expanded', !expanded);
   menuBtnOpen.classList.toggle('is-open');
   mobileMenu.classList.toggle('is-hiden');
   body.classList.toggle('overflov');
 });
-
-// menuBtnOpen.addEventListener('click', () => {
-//   menuBtnOpen.classList.toggle('is-open');
-//   mobileMenu.classList.toggle('is-hiden');
-//   body.classList.toggle('overflov');
-// });
